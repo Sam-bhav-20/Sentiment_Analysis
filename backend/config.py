ca70465f07b109340cb26e364991c1ae787d2c3a
soumik_ghosh <soumikghosh5225@gmail.com>
2024-03-21 23:12:56 +0530

    Backend Setup

---
+++ backend/config.py
@@ -0,0 +1,25 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    prod=ProductionConfig
+)
+
+# key = Config.SECRET_KEY
